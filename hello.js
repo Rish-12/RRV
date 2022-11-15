371144800f46e08e682b5f041086e6a291b5a9dd
Rishav <F20220738@pilani.bits-pilani.ac.in>
2022-11-15 17:33:39 +0000

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,11 @@
+const oneLinerJoke = require('one-liner-joke');
+const cowsay = require("cowsay");
+
+var getRandomJoke = oneLinerJoke.getRandomJoke();
+const cowsay = require("cowsay")
+console.log(getRandomJoke.body)
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oo",
+    T : "U "
+}));
